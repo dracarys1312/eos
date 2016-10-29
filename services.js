00140a9e093d0ae0c3590e8556241d4a6a0916cc
Pt <dracarys1312@gmail.com>
2016-10-29 20:46:38 +0700

    change space in mongo uri

--- services.js
+++ services.js
@@ -3,8 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 module.exports = function (app) {
 
     app.use(function (request, response, next) {
-        MongoClient.connect('mongodb://admin:admin@ds137267.mlab.com:37267/quizzes
-', function (error, connection) {
+        MongoClient.connect('mongodb://admin:admin@ds137267.mlab.com:37267/quizzes', function (error, connection) {
             if (error) {
                 return next(error);
             }
