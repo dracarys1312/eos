020bc2e0b3cdfd0dfe3733ebe2892da943f76920
Pt <dracarys1312@gmail.com>
2016-10-29 20:45:51 +0700

    change mongo uri

--- services.js
+++ services.js
@@ -3,7 +3,8 @@ const MongoClient = require('mongodb').MongoClient;
 module.exports = function (app) {
 
     app.use(function (request, response, next) {
-        MongoClient.connect('mongodb://localhost:27017/test', function (error, connection) {
+        MongoClient.connect('mongodb://admin:admin@ds137267.mlab.com:37267/quizzes
+', function (error, connection) {
             if (error) {
                 return next(error);
             }
